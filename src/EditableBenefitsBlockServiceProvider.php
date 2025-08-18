654350da10637f312f972acd69c446a9c32e8d34
Labrock <vladimirpeshkov5@yandex.ru>
2025-08-18 14:56:58 +0300

    initial commit

---
+++ src/EditableBenefitsBlockServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace GIS\EditableBenefitsBlock;
+
+
+use GIS\EditableBlocks\Traits\ExpandBlocksTrait;
+use GIS\Fileable\Traits\ExpandTemplatesTrait;
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+
+class EditableBenefitsBlockServiceProvider extends ServiceProvider
+{
+    use ExpandBlocksTrait, ExpandTemplatesTrait;
+
+    public function register(): void
+    {
+        // Config
+        $this->mergeConfigFrom(__DIR__ . "/config/editable-benefits-block.php", "editable-benefits-block");
+    }
+
+    public function boot(): void
+    {
+        // Views
+        $this->loadViewsFrom(__DIR__ . "/resources/views", "ebb");
+        // Livewire
+        $this->addLivewireComponents();
+        // Config
+        $this->expandConfiguration();
+    }
+
+    protected function addLivewireComponents(): void
+    {
+        $component = config("editable-benefits-block.customBenefitsComponent");
+    }
+
+    protected function expandConfiguration(): void
+    {
+        $ebb = app()->config["editable-benefits-block"];
+        $this->expandTemplates($ebb);
+        $this->expandBlocks($ebb);
+    }
+}
